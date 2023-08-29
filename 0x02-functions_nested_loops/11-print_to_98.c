4e9a578d531b968a89c90082e3ac4541dbba6a7f
Onyeije onyinyechi Doris <onyeijedoriso1@gmail.com>
2023-08-28 19:59:28 -0700

    memset

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-	#include "main.h"
+#include "main.h"
 
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
 
-	/**
-	 * print_to_98 - prints all natural numbers from n to 98,
-	 * followed by a new line
-	 * @n: print from this number
-	 */
-
-	void print_to_98(int n)
+void print_to_98(int n)
+{
+	int i;
+	
+	if (n >= 0 && n = 98)
 	{
-		int i, j;
-
-
-		if (n <= 98)
+		for (i = n; i <= 98; i++)
 		{
-			for (i = n; i <= 98; i++)
-			{
-				if (i != 98)
-					printf("%d, ", i);
+			printf("%d, ", i);
 				else if (i == 98)
 					printf("%d\n", i);
 			}
